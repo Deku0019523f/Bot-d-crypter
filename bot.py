8a6f109bef7ed69bd4252d0b7db27c08c60fd837
Alec <alecbitty@gmail.com>
2025-05-28 11:41:23 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
-from handlers.decrypt import handle_document
-from loader import dp, bot
+from handlers.decrypt import decrypt  # ✅ Corrigé ici
+
+from loader import dp
 
 # Enregistrement du handler
-dp.register_message_handler(handle_document, content_types=types.ContentType.DOCUMENT)
+dp.register_message_handler(decrypt, content_types=types.ContentType.DOCUMENT)
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
