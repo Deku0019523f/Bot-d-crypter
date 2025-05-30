7aad57b31d24d193d2933db42dc403111f951e79
Alec <alecbitty@gmail.com>
2025-05-30 12:40:09 +0000

    Update predictor.py

--- api/predictor.py
+++ api/predictor.py
@@ -1,2 +1,10 @@
-def predict_match():
-    return "Score prédictif : 2-1"
+def predict_match(team1, team2):
+    # Ex. simplifié : on prédit match nul
+    return {
+        "winner": "Draw",
+        "score": "1-1",
+        "goals": {
+            team1: 1,
+            team2: 1
+        }
+    }
